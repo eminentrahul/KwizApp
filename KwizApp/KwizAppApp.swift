50df62da03b9b08825ea8a9a63bfb7a2d5703fb1
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2025-05-19 20:15:17 +0530

    Initial Commit

---
+++ KwizApp/KwizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KwizAppApp.swift
+//  KwizApp
+//
+//  Created by Rahul Ravi Prakash on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct KwizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
